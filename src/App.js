f7a771c6590742112f55cbf90c92962ed732ae8a
Osman Oz <ali.osman.oz@hotmail.com>
2023-03-13 08:39:37 +0300

    todo app v1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 function App() {
   const [todos, setTodos] = useState([]);
   const [todoText, setTodoText] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
+  const [editTodoId, setEditTodoId] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,17 +17,48 @@ function App() {
     const hasTodo = todos.find((todo) => todo.text === todoText);
 
     if (hasTodo) {
-      alert("Aynı ad ile göreviniz var");
+      alert("You have the todo already");
       return;
     }
 
-    const newTodo = {
-      id: String(new Date().getTime()),
-      text: todoText,
-      isDone: false,
+    if (isEdit === true) {
+      const editTodo = todos.find((todo) => todo.id === editTodoId);
+      const newEditTodo = {
+        ...editTodo,
+        text: todoText,
+      };
+
+      const filteredEdit = todos.filter((todo) => todo.id !== editTodoId);
+      setTodos([...filteredEdit, newEditTodo]);
+      setTodoText("");
+      setIsEdit(false);
+    } else {
+      const newTodo = {
+        id: String(new Date().getTime()),
+        text: todoText,
+        isDone: false,
+      };
+      setTodos([...todos, newTodo]);
+      setTodoText("");
+    }
+  };
+
+  const changeIsDone = (id) => {
+    const changedTodo = todos.find((todo) => todo.id === id);
+    const newChangeTodo = {
+      ...changedTodo,
+      isDone: !changedTodo.isDone,
     };
-    setTodos([...todos, newTodo]);
-    setTodoText("");
+
+    const changedTodoFilter = todos.filter((todo) => todo.id !== id);
+
+    setTodos([...changedTodoFilter, newChangeTodo]);
+  };
+
+  const handleDelete = (id) => {
+    const deletedTodo = todos.filter((todo) => todo.id !== id);
+    console.log("deletedTodo", deletedTodo);
+    setTodos(deletedTodo);
   };
 
   return (
@@ -40,8 +73,11 @@ function App() {
           value={todoText}
           onChange={(e) => setTodoText(e.target.value)}
         />
-        <button className="btn btn-primary" type="submit">
-          Add
+        <button
+          className={`btn btn-${isEdit === true ? "success" : "primary"}`}
+          type="submit"
+        >
+          {isEdit === true ? "Save" : "Add"}
         </button>
       </form>
       {todos.length === 0 ? (
@@ -50,9 +86,40 @@ function App() {
         </div>
       ) : (
         todos.map((item) => (
-          <div>
-            <div className="alert alert-secondary" role="alert">
+          <div key={item.id}>
+            <div
+              className={`alert alert-${
+                item.isDone === false ? "secondary" : "success"
+              } d-flex justify-content-between align-items-center py-2`}
+              role="alert"
+            >
               {item.text}
+              <div>
+                <button
+                  onClick={() => {
+                    setIsEdit(true);
+                    setEditTodoId(item.id);
+                    setTodoText(item.text);
+                  }}
+                  className="btn btn-sm btn-success py-0"
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => handleDelete(item.id)}
+                  className="btn btn-sm btn-danger py-0 mx-1"
+                >
+                  Delete
+                </button>
+                <button
+                  className={`btn btn-sm btn-${
+                    item.isDone === false ? "secondary" : "success"
+                  } py-0`}
+                  onClick={() => changeIsDone(item.id)}
+                >
+                  {item.isDone === false ? "Done" : "UnDone"}
+                </button>
+              </div>
             </div>
           </div>
         ))
---
+++ src/Components/Todos.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Todos = () => {
+  return (
+    <div>
+      <h4>Todo</h4>
+    </div>
+  );
+};
+export default Todos;
